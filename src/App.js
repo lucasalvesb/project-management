5d19b5b8bbcd17d3519e24dd8f5fcaacec87862f
Lucas Alves <lucasalves94@gmail.com>
2022-12-10 10:23:15 -0300

    routing done for all the pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+// styles
 import './App.css'
+
+
+// pages and components
+import Dashboard from './pages/dashboard/Dashboard'
+import Create from './pages/create/Create'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import Project from './pages/project/Project'
+
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <div className="container">
+          <Route path="" element={<Home />}/>
+          <Route path="/create" element={<Home />}/>
+          <Route path="/project/:id" element={<Project />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/signup" element={<Signup />}/>
+        </div>
+      </Routes>
     </div>
   );
 }
