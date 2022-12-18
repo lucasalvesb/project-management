a1b0168b06432a51c2056d9f942f304540a44a11
Lucas Alves <lucasalves94@gmail.com>
2022-12-17 21:38:14 -0300

    showing users online

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,10 +1,23 @@
 //styles
 import './Create.css'
 
+import { useState } from 'react'
+
 export default function Create() {
+
+const [name, setName] = useState('')
+const [details, setDetails] = useState('')
+const [dueDate, setDueDate] = useState('')
+const [category, setCategory] = useState('')
+const [assignedUsers, setAssignedUsers] = useState([])
+
+
     return (
-        <div>
-            Create
+        <div className="create-form">
+            <h2 className="page-title">Create a new project</h2>
+            <form>
+
+            </form>
         </div>
     )
 }
