506f73286539504893eedfb2e7a83aad11485ed4
Lucas Alves <lucasalves94@gmail.com>
2022-12-21 17:52:02 -0300

    create page skeleton and basic functions implemented

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -12,11 +12,53 @@ const [category, setCategory] = useState('')
 const [assignedUsers, setAssignedUsers] = useState([])
 
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(name, details, dueDate)
+    }
+
     return (
         <div className="create-form">
             <h2 className="page-title">Create a new project</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Project name:</span>
+                    <input
+                    required 
+                    type="text"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                    />
+                </label>
 
+                <label>
+                    <span>Project details:</span>
+                    <textarea
+                    required 
+                    type="text"
+                    onChange={(e) => setDetails(e.target.value)}
+                    value={details}
+                    ></textarea>
+                </label>
+
+                <label>
+                    <span>Set due date:</span>
+                    <input
+                    required 
+                    type="date"
+                    onChange={(e) => setDueDate(e.target.value)}
+                    value={dueDate}
+                    />
+                </label>
+                <label>
+                    <span>Project category</span>
+                </label>
+
+                <label>
+                    <span>Assign to:</span>
+                </label>
+
+                <button className="btn">Add project</button>
             </form>
         </div>
     )
