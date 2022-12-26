ae8740f53984fac75673898910e7a0c366727ad5
Lucas Alves <lucasalves94@gmail.com>
2022-12-26 19:18:58 -0300

    fixed small typo

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -7,7 +7,7 @@ import './ProjectList.css'
 
 export default function ProjectList( { projects }) {
   return (
-    <div>
+    <div className="project-list">
       {projects.length === 0 && <p>No projects yet!</p>}
       {projects.map(project => (
         <Link to={`/project/${project.id}`} key={project.id}>
